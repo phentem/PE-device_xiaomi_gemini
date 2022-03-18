5878bfaf9dfa18d863f3e140451351fce46b8e11
phentem <phentem@gmail.com>
2022-03-19 01:13:47 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_gemini.mk
+    $(LOCAL_DIR)/spark_gemini.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_gemini-user \
-    aosp_gemini-userdebug \
-    aosp_gemini-eng
+    spark_gemini-user \
+    spark_gemini-userdebug \
+    spark_gemini-eng
